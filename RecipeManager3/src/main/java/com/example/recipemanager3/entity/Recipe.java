f79521955e4f9b0cb637d9c278bbf8a80e95ec7c
bearb <bearb@host.docker.internal>
2025-08-31 16:14:57 +0900

    タイトル文字制限追加

--- RecipeManager3/src/main/java/com/example/recipemanager3/entity/Recipe.java
+++ RecipeManager3/src/main/java/com/example/recipemanager3/entity/Recipe.java
@@ -2,6 +2,7 @@ package com.example.recipemanager3.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,13 +11,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Recipe {
+	
     private int id;
+    
+    @Size(max = 20, message = "タイトルは20文字以内で入力してください")
     private String title;
+    
     private String description;
+    
     private int categoryId;
+    
     private int userId;  // 忘れていたら追加
     
     private LocalDateTime createdAt; // ★追加
+    
     private LocalDateTime updatedAt; // ★追加
 
 }
